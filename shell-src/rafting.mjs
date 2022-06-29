59e715daba1fd122cf970362fd7069eb21fdbe8e
haruyuki@gamibook-wsl <haruyuki.program@gmail.com>
2022-06-30 00:01:31 +0900

    👍 川下りをちょっと強化した

--- shell-src/rafting.mjs
+++ shell-src/rafting.mjs
@@ -1,5 +1,14 @@
 #!/usr/bin/env zx
 
+const rp = (text, n) => {
+  let res = ''
+  for(let i = 0; i < n; i++) {
+    res += text
+  }
+  return res
+}
+
+// ANSI Escape functions
 /** cursol move up */
 const cu = (n = 1) => { return `\u001b[${n}A`}
 /** cursol move down */
@@ -10,18 +19,22 @@ const cr = (n = 1) => { return `\u001b[${n}C`}
 const cl = (n = 1) => { return `\u001b[${n}D`}
 /** cursol move next line head */
 const nlh = (n = 1) => { return `\u001b[${n}E` }
+/** cursol move to x, y */
+const cmv = (x, y) => { return `\u001b[${y};${x}H` }
 /** cursol move previous line head */
 const plh = (n = 1) => { return `\u001b[${n}F` }
 /** delete line */
 const dl = (n = 2) => { return `\u001b[${n}K` }
+/** delete screen */
+const ds = (n = 2) => { return `\u001b[${n}J` }
 
 // 川下りゲーム書く
 
-echo('--------------川下りゲーム--------------')
-echo('        Enterを押して設定を始める        ')
+echo(`${ds()}`)
+echo(`${cmv(1, 1)}${chalk.blue('*')}${cr(40)}${chalk.blue('*')}${nlh(10)}${chalk.blue('*')}${cr(40)}${chalk.blue('*')}${cmv(1, 12)}`)
+await sleep(1000)
+echo(`${cmv(16, 5)}川下りゲーム`)
+echo(`${cmv(11, 6)}Enter を押してスタート`)
+echo(`${cmv(1, 12)}`)
 
 await question()
-
-echo(`\u001b[0Ftest`)
-
-echo(`${cu()}${cu()}${dl()}${cu()}${dl()}test`)
